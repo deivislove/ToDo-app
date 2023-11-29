5ee430e11ca88b12fa35ec99bf045d24cfdace8c
Devis dHora <davi.hamorim@eaportal.org>
2023-11-29 01:17:06 -0300

    exclusão de tarefas e finalização do projeto

--- index.js
+++ index.js
@@ -17,6 +17,18 @@ app.use(express.urlencoded({
 app.use(express.json())
 
 //rotas
+app.get('/limparTarefas', (requisicao, resposta) => {
+    const sql = 'DELETE FROM tarefas'
+
+    conexao.query(sql, (erro) => {
+        if (erro) {
+            return console.log(erro)
+        }
+
+        resposta.redirect('/')
+    })
+})
+
 app.post('/excluir', (requisicao, resposta) => {
     const id = requisicao.body.id
 
@@ -41,7 +53,7 @@ app.post('/completar', (requisicao, resposta) => {
         SET completa = '1'
         WHERE id = ${id}
     `
-    
+
     conexao.query(sql, (erro) => {
         if (erro) {
             return console.log(erro)
